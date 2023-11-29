908c2d9ac5e208f333e548f6677e4e01c21d06f8
franclocat <148209747+franclocat@users.noreply.github.com>
2023-11-29 15:53:47 +0100

    javascript update for track buttons

--- scripts/setupscript.js
+++ scripts/setupscript.js
@@ -1,35 +1,42 @@
-// List of track names
-const accTracks = [
-    'Zandvoort',
-    'Misano',
-    'Brands Hatch',
-    'Silverstone',
-    'Circuit de Barcelona-Catalunya',
-    'Paul Ricard',
-    'Hungaroring',
-    'Nürburgring',
-    'Spa-Francorchamps',
-    'Monza',
-    'Circuit de Nevers Magny-Cours',
-    'Circuit Zolder',
-    'Circuit de Spa-Francorchamps 24H',
-    'Suzuka Circuit',
-    'Circuit de Barcelona-Catalunya 2019',
-    'Laguna Seca',
-    'Kyalami Grand Prix Circuit',
-    'Mount Panorama Circuit',
-    'Suzuka Circuit East Course',
-    'Suzuka Circuit West Course'
-  ];
-const buttonContainer = document.getElementById('tracks');
-
-// Create buttons for each track
-accTracks.forEach(track => {
-  const button = document.createElement('button');
-  button.id = track; // Set the button ID to the track name
-  button.innerText = track; // Display the track name on the button
-  buttonContainer.appendChild(button); // Append button to the container
-
-  // Set background image for each button (assuming images are named accordingly)
-  button.style.backgroundImage = `url('imgs/${track}.jpg')`;
-});
+document.addEventListener("DOMContentLoaded", function () {
+    // List of track names
+    const accTracks = [
+      'Zandvoort',
+      'Misano',
+      'Brands Hatch',
+      'Silverstone',
+      'Circuit de Barcelona-Catalunya',
+      'Paul Ricard',
+      'Hungaroring',
+      'Nürburgring',
+      'Spa-Francorchamps',
+      'Monza',
+      'Circuit de Nevers Magny-Cours',
+      'Circuit Zolder',
+      'Circuit de Spa-Francorchamps 24H',
+      'Suzuka Circuit',
+      'Circuit de Barcelona-Catalunya 2019',
+      'Laguna Seca',
+      'Kyalami Grand Prix Circuit',
+      'Mount Panorama Circuit',
+      'Suzuka Circuit East Course',
+      'Suzuka Circuit West Course'
+    ];
+  
+    const tracksContainer = document.getElementById('tracks');
+  
+    // Create buttons for each track and append them to the 'tracks' div
+    accTracks.forEach(track => {
+      const button = document.createElement('button');
+      button.id = track; // Set the button ID to the track name
+      button.innerText = track; // Display the track name on the button
+      tracksContainer.appendChild(button); // Append button to the 'tracks' div
+  
+      // Set background image for each button (assuming images are named accordingly)
+      button.style.backgroundImage = `url('imgs/${track}.jpg')`;
+      button.style.backgroundSize = 'cover'; // Adjust background size to cover the button
+      button.style.backgroundPosition = 'center'; // Center the background image
+      button.style.backgroundRepeat = 'no-repeat'; // Prevent background image from repeating
+    });
+  });
+  
