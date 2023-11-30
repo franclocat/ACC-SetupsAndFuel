8e5002e493f04784013605641aea7409c9cdbbf5
franclocat <148209747+franclocat@users.noreply.github.com>
2023-11-30 16:08:58 +0100

    updated calculator with 2 forms of calculating fuel

--- scripts/calcscript.js
+++ scripts/calcscript.js
@@ -1,28 +1,39 @@
-if (lapsSelect.checked) {
-    document.getElementById("changeableLengthType").innerHTML = "Racing Laps";
+function getChangeableElement() {
+    var timeSelect = document.getElementById("timeSelect");
+    var lapsSelect = document.getElementById("laps");
+    if (timeSelect.checked) {
+        document.getElementById("changeableLengthType").innerHTML = "Racing time in minutes";
+    } else if (lapsSelect.checked) {
+        document.getElementById("changeableLengthType").innerHTML = "Racing Laps";
+    } else {
+        document.getElementById("changeableLengthType").innerHTML = "Select a length method";
+    }
 }
+setInterval(getChangeableElement,0);
+
 
 function calculateFuel() {
-
-    var TR = parseInt(document.getElementById("raceLength").value);
-    var minutesInSeconds = parseInt(document.getElementById("minutes").value) * 60;
-    var seconds = parseInt(document.getElementById("seconds").value);
-    var FL = parseFloat(document.getElementById("fuelPerLap").value);
-    var result = parseInt(((TR * 60) / (minutesInSeconds + seconds) ) * FL + (2 * FL));
+    var raceLength = parseInt(document.getElementById("raceLength").value);
+    var fuelPerLap = parseFloat(document.getElementById("fuelPerLap").value);
 
     var timeSelect = document.getElementById("timeSelect");
     var lapsSelect = document.getElementById("laps");
+    var resElement = document.getElementById("res");
 
     if (timeSelect.checked) {
-        var result = parseInt(((TR * 60) / (minutesInSeconds + seconds) ) * FL + (2 * FL));
+        lapsSelect.checked = false; // Uncheck lapsSelect if timeSelect is checked
+        // Rest of the calculation remains the same...
+        var minutesInSeconds = parseInt(document.getElementById("minutes").value) * 60;
+        var seconds = parseInt(document.getElementById("seconds").value);
+        var totalTimeInSeconds = minutesInSeconds + seconds;
+        var resultTime = parseInt(((raceLength * 60) / totalTimeInSeconds) * fuelPerLap + 2 * fuelPerLap);
+        resElement.innerHTML = isNaN(resultTime) ? "0 L" : resultTime + " L";
     } else if (lapsSelect.checked) {
-        var result = parseInt(TR * FL);
-    }
-    
-
-    if (isNaN(result)) {
-        document.getElementById("res").innerHTML = "0 L";  
-    } else {
-        document.getElementById("res").innerHTML = result + " L";
+        timeSelect.checked = false; // Uncheck timeSelect if lapsSelect is checked
+        // Rest of the calculation for lapsSelect...
+        var racingLaps = raceLength; // Assuming raceLength represents number of laps
+        // Calculate fuel based on laps logic
+        var resultLaps = parseInt(racingLaps * fuelPerLap);
+        resElement.innerHTML = isNaN(resultLaps) ? "0 L" : resultLaps + " L";
     }
 }
