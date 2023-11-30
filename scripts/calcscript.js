11df2a666c9236c07f26c2bb6a3e6e7c6e249004
franclocat <148209747+franclocat@users.noreply.github.com>
2023-11-30 13:40:09 +0100

    updated calculator

--- scripts/calcscript.js
+++ scripts/calcscript.js
@@ -1,9 +1,28 @@
+if (lapsSelect.checked) {
+    document.getElementById("changeableLengthType").innerHTML = "Racing Laps";
+}
+
 function calculateFuel() {
+
     var TR = parseInt(document.getElementById("raceLength").value);
     var minutesInSeconds = parseInt(document.getElementById("minutes").value) * 60;
     var seconds = parseInt(document.getElementById("seconds").value);
     var FL = parseFloat(document.getElementById("fuelPerLap").value);
     var result = parseInt(((TR * 60) / (minutesInSeconds + seconds) ) * FL + (2 * FL));
 
-    document.getElementById("res").innerHTML = result + " L";
+    var timeSelect = document.getElementById("timeSelect");
+    var lapsSelect = document.getElementById("laps");
+
+    if (timeSelect.checked) {
+        var result = parseInt(((TR * 60) / (minutesInSeconds + seconds) ) * FL + (2 * FL));
+    } else if (lapsSelect.checked) {
+        var result = parseInt(TR * FL);
+    }
+    
+
+    if (isNaN(result)) {
+        document.getElementById("res").innerHTML = "0 L";  
+    } else {
+        document.getElementById("res").innerHTML = result + " L";
+    }
 }
